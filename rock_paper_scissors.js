54e59ce78fc9cbb385caf183ee5261a6d9edf906
Benjamin Benge <bengebenjamin010@gmail.com>
2024-08-23 13:52:24 +0300

    Add human choice and computer choice

---
+++ rock_paper_scissors.js
@@ -0,0 +1,30 @@
+const tools = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  console.log(tools[Math.floor(Math.random() * tools.length)]);
+}
+
+getComputerChoice();
+
+function getHumanChoice() {
+  const user = prompt("Rock, Paper or Scissors");
+
+  console.log(user);
+}
+
+getHumanChoice();
+
+const humanScore = 0
+const computerScore = 0
+
+function playRound(humanChoice,computerChoice){
+if(humanChoice = Paper && computerChoice = Rock)
+}
+
+   const humanSelection= getHumanChoice();
+  const  computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection)
+
+  
+
